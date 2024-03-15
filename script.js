16595f591fb776d409f5d1076e4245bcf98c628b
Nahid Ul Kabir <rafi.eee.1999@gmail.com>
2024-03-16 00:02:40 +0600

    add resume download add profile picture add icon and link

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
-const typed = new Typed('.auto_type',{
-  string:["Frontend Developer" , "MERN Starck Developer" , "Codding and Sleep"],
-  typeSpeed:150,
-  backSpeed:150,
-})
+var typed = new Typed(".auto_type", {
+  strings: ["Software Engineer", "MERN Stack Developer", "React Developer"],
+  typeSpeed: 150,
+  backSpeed: 150,
+});
 
-document.addEventListener('mousemove', function(e) {    
-    var tracker = document.querySelector('.tracker');
-    var trackerring = document.querySelector('.trackerring');
-    var x = e.clientX;
-    var y = e.clientY;
-    console.log(x,y)
-    // tracker.style.left = tracker-ring = x + 'px';
-    // tracker.style.top = tracker-ring = y + 'px';
-    tracker.style.cssText = trackerring.style.cssText = "left: " + x +"px ; top: " + y + "px;"
-  });
-
+document.addEventListener("mousemove", function (e) {
+  var tracker = document.querySelector(".tracker");
+  var trackerring = document.querySelector(".trackerring");
+  var x = e.clientX;
+  var y = e.clientY;
+  console.log(x, y);
+  // tracker.style.left = tracker-ring = x + 'px';
+  // tracker.style.top = tracker-ring = y + 'px';
+  tracker.style.cssText = trackerring.style.cssText =
+    "left: " + x + "px ; top: " + y + "px;";
+});
